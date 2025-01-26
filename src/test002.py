384c9501f961580a277c0b9e88ad1eff91a84332
siawase <aratake-kouji@n03.itscom.net>
2025-01-26 23:38:33 +0000

    1/27 update2

--- src/test002.py
+++ src/test002.py
@@ -2,7 +2,7 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='パラメタをargparseで定義するプログラム')
-    parser.add_argument('--param1', type=int, help='整数型のパラメタ1')
+    parser.add_argument('param1', type=int, help='整数型のパラメタ1')
     parser.add_argument('--param2', type=str, help='文字列型のパラメタ2')
     args = parser.parse_args()
 
